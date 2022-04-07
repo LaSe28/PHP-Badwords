6142003f906694a167d4e46898555ae407945542
LaSe28 <mattia.las28@gmail.com>
2022-04-07 17:11:52 +0200

    end

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form styleaction="">
+    <input name="censored" type="text">
+    <input type="submit" value="Censura!">
+  </form>
+  <?php 
+    $testo = 'you made me a, you made me a believer, believer
+    (pain!)
+    You break me down and build me up, believer, believer
+    (pain!)
+    oh, let the bullets fly, oh, let them rain
+    my life, my love, my drive, it came from...
+    (Pain!)
+    you made me a, you made me a believer, believer';
+    $bad_word = $_GET['censored'];
+    $censura = '***';
+    $censored_text = str_replace($bad_word, $censura, $testo);
+    echo $censored_text;
+    echo ' (' . strlen($censored_text) . ')'
+  ?>
+</body>
+</html>
